f9ba196de8072bee4b368316051560b6fe74f8ee
Ahmed Abdou <hakam4all@yahoo.com>
2023-06-20 23:48:16 -0700

    UPDATE 20-JUN

--- main.js
+++ main.js
@@ -43,8 +43,15 @@ step2.addEventListener('click', function (event) {
 
 next2.addEventListener('click', function (event) {
     event.preventDefault();
-    // let numberOfPeople = Number(people2.value);
-    // let bookingDate = bookDate2.value;
     secondBooking.style.display = 'none';
 });
 
+/* Calculating the map height based on the screen width and aspect ration */
+
+window.addEventListener('resize', function() {
+    var div = document.querySelector('.booking-content');
+    div.style.height = div.offsetWidth * (6 / 16) + 'px'; // Calculate the height based on the width and aspect ratio
+  });
+
+  // Trigger the resize event initially to set the initial height
+  window.dispatchEvent(new Event('resize'));
