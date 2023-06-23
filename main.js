acfcb073abcc46171e4bc3a2848f6405bea17654
Ahmed Abdou <hakam4all@yahoo.com>
2023-06-22 20:42:47 -0700

    Filters issue solved

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ secondBooking.style.display = 'none';
 
 // Step 1 
 let x = 0;
-step1.addEventListener('click', function (event) {
-    event.preventDefault();
+step1.addEventListener('click', function (e) {
+    e.preventDefault();
     secondBooking.style.display = 'none';
 
     if (x === 0) {
@@ -17,8 +17,8 @@ step1.addEventListener('click', function (event) {
     }
 });
 
-next1.addEventListener('click', function (event) {
-    event.preventDefault();
+next1.addEventListener('click', function (e) {
+    e.preventDefault();
     // let numberOfPeople = Number(people.value);
     // let bookingDate = bookDate.value;
     stepOne.style.display = 'none';
@@ -28,8 +28,8 @@ next1.addEventListener('click', function (event) {
 
 // Step 2 
 let y = 0;
-step2.addEventListener('click', function (event) {
-    event.preventDefault();
+step2.addEventListener('click', function (e) {
+    e.preventDefault();
     stepOne.style.display = 'none';
 
     if (y === 0) {
@@ -41,8 +41,8 @@ step2.addEventListener('click', function (event) {
     }
 });
 
-next2.addEventListener('click', function (event) {
-    event.preventDefault();
+next2.addEventListener('click', function (e) {
+    e.preventDefault();
     secondBooking.style.display = 'none';
 });
 
@@ -56,10 +56,8 @@ window.addEventListener('resize', function () {
 // Trigger the resize event initially to set the initial height
 window.dispatchEvent(new Event('resize'));
 
-
-
-next1.addEventListener('click', function (event2) {
-    event2.preventDefault();
+next1.addEventListener('click', function (e) {
+    e.preventDefault();
     let noPeople = Number(people.value);
     if (noPeople > 0) {
         let resetBackGround = document.querySelectorAll('.booking-content p');
@@ -91,6 +89,131 @@ next1.addEventListener('click', function (event2) {
             spacenumber[n].style.backgroundColor = 'grey';
         }
     }
+}
+
+)
+
+
+/* The Pos/Neg Button Code for Power Outlet */
+
+
+let posOutlet = document.querySelector('#filter1 .pos');
+let negOutlet = document.querySelector('#filter1 .neg');
+let countOutlet = document.querySelector('#filter1 .counter');
+
+
+posOutlet.addEventListener('click', function (e) {
+    e.preventDefault();
+    countOutlet.innerHTML = "1"
+})
+
+negOutlet.addEventListener('click', function (e) {
+    e.preventDefault();
+    countOutlet.innerHTML = "0"
+})
+
+
+
+/* The Pos/Neg Button Code for Monitor */
+
+
+let posMonitor = document.querySelector('#filter3 .pos');
+let negMonitor = document.querySelector('#filter3 .neg');
+let countMonitor = document.querySelector('#filter3 .counter');
+
+
+posMonitor.addEventListener('click', function (e) {
+    e.preventDefault();
+    countMonitor.innerHTML = "1"
+})
+
+negMonitor.addEventListener('click', function (e) {
+    e.preventDefault();
+    countMonitor.innerHTML = "0"
+})
+
+
+/* The Pos/Neg Button Code for Projector */
+
+
+let posProjector = document.querySelector('#filter4 .pos');
+let negProjector = document.querySelector('#filter4 .neg');
+let countProjector = document.querySelector('#filter4 .counter');
+
+
+posProjector.addEventListener('click', function (e) {
+    e.preventDefault();
+    countProjector.innerHTML = "1"
+})
+
+negProjector.addEventListener('click', function (e) {
+    e.preventDefault();
+    countProjector.innerHTML = "0"
+})
+
+
+/* The Pos/Neg Button Code for Computers */
+
+
+let posComputer = document.querySelector('#filter2 .pos');
+let negComputer = document.querySelector('#filter2 .neg');
+let countComputer = document.querySelector('#filter2 .counter');
+let counterComputer = parseInt(countComputer.textContent);
+
+posComputer.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (counterComputer < 8) {
+        counterComputer++;
+    }
+    countComputer.textContent = counterComputer.toString();
+})
+
+negComputer.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (counterComputer > 0) {
+        counterComputer--;
+    }
+    countComputer.textContent = counterComputer.toString();
+
+})
+
+
+/* The Pos/Neg Button Code for Chairs */
+
+
+let posChairs = document.querySelector('#filter5 .pos');
+let negChairs = document.querySelector('#filter5 .neg');
+let countChairs = document.querySelector('#filter5 .counter');
+let counterChairs = parseInt(countChairs.textContent);
+
+posChairs.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (counterChairs < 8) {
+        counterChairs++;
+    }
+    countChairs.textContent = counterChairs.toString();
+})
+
+negChairs.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (counterChairs > 0) {
+        counterChairs--;
+    }
+    countChairs.textContent = counterChairs.toString();
+
+})
+
+
+next2.addEventListener('click', function (event5) {
+    let noPeople = Number(people.value);
+
+    if (countOutlet > 1) {
+        let resetBackGround = document.querySelectorAll('.booking-content p');
+        for (let r = 0; r < resetBackGround.length; r++) {
+            resetBackGround[r].style.backgroundColor = ' rgba(255, 166, 0, 0.4)';
+        }
+    }
+
 }
 
 )
