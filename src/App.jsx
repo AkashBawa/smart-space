d81a85084248f562a60625214559fffdbcdc2c36
AkashBawa <bawa@2027@gmail.com>
2023-07-01 00:08:41 -0700

    [firebase]: environment complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,18 @@ import NotFound from './pages/notFound';
 import ProtectedRoute from './utils/protectedRoutes';
 import Home from './pages/home';
 
-import { addDoc, collection } from "@firebase/firestore"
-import { firestore } from "./utils/fireStore";
+import { addDoc, collection, getDocs, doc } from "@firebase/firestore"
+import { firestore, addDataToCollection, getAllDataFromCollection } from "./utils/fireStore";
+import  collectionsName from './constants/firebaseCollection';
 
 function App() {
 
   const handleSubmit = async () => {
-    console.log("indide handle submit")
-    const ref = collection(firestore, "users") // Firebase creates this automatically
- 
-    let data = {
-        name: "from firebase",
-        email: "akash@firebase.com"
-    }
-    debugger;
-    try {
-        const add = await addDoc(ref, data);
-        console.log(add);
-    } catch(err) {
-        console.log(err)
+    const data = await getAllDataFromCollection( 'users');
+    if(data) {
+      data.forEach((user) => {
+        console.log(user.data());
+      })
     }
   }
   return (
--- src/utils/fireStore.jsx
+++ src/utils/fireStore.jsx
@@ -1,15 +1,53 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { addDoc, collection, getDocs, getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
-};
+const getConfiguration = () => {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: "samart-space",
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
+  };
+  return firebaseConfig;
+}
 
-const app = initializeApp(firebaseConfig);
+function functionInitApp() {
+  const fireBaseConfig = getConfiguration();
+  console.log(fireBaseConfig);
+  const app = initializeApp(fireBaseConfig, {
+    experimentalForceLongPolling: true, // this line
+    useFetchStreams: false, // and this line
+  });
+  return app;
+}
+
+const app = functionInitApp();
 export const firestore = getFirestore(app)
+
+
+export const  addDataToCollection = async (collectionName, data) => {
+
+  try {
+    const ref = collection(firestore, collectionName) 
+    const docRef = await addDoc(ref, data);
+    console.log(docRef.id);
+    return docRef;
+  } catch (err) {
+    console.log("Error in adding new document", err)
+  }
+} 
+
+export const  getAllDataFromCollection = async (collectionName) => {
+
+  try {
+    const ref = collection(firestore, collectionName) 
+    const docRef = await getDocs(ref);
+    return docRef
+  } catch (err) {
+    console.log("Error in getting new document", err)
+  }
+  
+} 
