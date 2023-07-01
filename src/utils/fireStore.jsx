a79fbaeb31c25dd0f634c2a8b2ed961b27101a92
somchai sik <somchai7691@gmail.com>
2023-06-30 23:37:54 -0700

    firestor auth

--- src/utils/fireStore.jsx
+++ src/utils/fireStore.jsx
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
@@ -12,4 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 export const firestore = getFirestore(app)
