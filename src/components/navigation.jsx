15877786ff9a7377ed371ba7ec14b3c7593205f3
somchai sik <somchai7691@gmail.com>
2023-07-02 02:21:41 -0700

    login,signup,qrpage,updated

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,33 +1,52 @@
 import { Link, Outlet } from "react-router-dom";
-import siteLogo from "./../public/Images/Logo/logo-Orange.png"
+import siteLogo from "./../public/Images/Logo/logo-Orange.png";
+import Home from "../pages/home";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Navigation = () => {
-    return (
-      <div>
+  const navigator = useNavigate();
 
-        <header className="navigation">
-          <img src={siteLogo} alt="" />
-              <ul>
-                  <li> <Link to="/home">Home</Link> </li>
-                  <li> <Link to="/booking">Booking</Link></li>
-                  <li>  <Link to="/reschedule">Reschedule</Link></li>
-                  {/* <li>  <Link to="/login">login</Link></li>
+  // functon to go home
+  const logoHome = () => {
+    navigator("/home");
+  }
+
+  return (
+    <div>
+      <header className="navigation">
+        <img src={siteLogo} className="logo" alt="" onClick={logoHome} />
+        <nav>
+          <ul className="nav-bar">
+            <li>
+              {" "}
+              <Link to="/home">Home</Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/booking">Booking</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/reschedule">Reschedule</Link>
+            </li>
+            {/* <li>  <Link to="/login">login</Link></li>
                   <li>  <Link to="/signup">signup</Link></li>
                   <li>  <Link to="/qr-page">QR-SCAN</Link></li> */}
-              </ul>
+          </ul>
+        </nav>
 
-              <div>
-                
-              </div>
-        </header>
-        
-        <div className="main">
-          <Outlet/>
+        <div>
+          <p>A</p>
         </div>
+      </header>
 
+      <div className="main">
+        <Outlet />
       </div>
-    );
-  }
-  
-  export default Navigation;
-  
+    </div>
+  );
+};
+
+export default Navigation;
