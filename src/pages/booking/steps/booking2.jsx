ac376e4985f2aa1b48583690b17c71d68d659c99
Ahmed Abdou <hakam4all@yahoo.com>
2023-07-20 08:30:49 -0700

    update the option list

--- src/pages/booking/steps/booking2.jsx
+++ src/pages/booking/steps/booking2.jsx
@@ -24,28 +24,28 @@ const Booking2 = (props) => {
 
     const existingBooking = props.existingBooking;
 
-    const [timeSlotes, setTimeSlotes] = useState ([
-        {startTime: 8, display: '8am - 9am'},
-        {startTime: 9, display: '9am - 10am'},
-        {startTime: 10, display: '10am - 11am'},
-        {startTime: 11, display: '11am - 12pm'},
-        {startTime: 12, display: '12pm - 1pm'},
-        {startTime: 13, display: '1pm - 2pm'},
-        {startTime: 14, display: '2pm - 3pm'},
-        {startTime: 15, display: '3pm - 4pm'},
-        {startTime: 16, display: '4pm - 5pm'},
-        {startTime: 17, display: '5pm - 6pm'},
-        {startTime: 18, display: '6pm - 7pm'},
-        {startTime: 19, display: '7pm - 8pm'},
+    const [timeSlotes, setTimeSlotes] = useState([
+        { startTime: 8, display: '8am - 9am' },
+        { startTime: 9, display: '9am - 10am' },
+        { startTime: 10, display: '10am - 11am' },
+        { startTime: 11, display: '11am - 12pm' },
+        { startTime: 12, display: '12pm - 1pm' },
+        { startTime: 13, display: '1pm - 2pm' },
+        { startTime: 14, display: '2pm - 3pm' },
+        { startTime: 15, display: '3pm - 4pm' },
+        { startTime: 16, display: '4pm - 5pm' },
+        { startTime: 17, display: '5pm - 6pm' },
+        { startTime: 18, display: '6pm - 7pm' },
+        { startTime: 19, display: '7pm - 8pm' },
     ]);
 
     useEffect(() => {
         window.addEventListener('resize', function () {
             var div = document.querySelector('.booking-content');
-            if(div) {
+            if (div) {
                 div.style.height = div.offsetWidth * (6 / 16) + 'px';
             }
-           
+
             // Calculate the height based on the width and aspect ratio
         });
 
@@ -53,9 +53,9 @@ const Booking2 = (props) => {
 
     const checkPowerOutlet = (currentTable) => {
 
-        if(powerOutlet == true && currentTable.hasPowerOutlet  && currentTable.hasPowerOutlet == true) {
+        if (powerOutlet == true && currentTable.hasPowerOutlet && currentTable.hasPowerOutlet == true) {
             return true;
-        } else if ( powerOutlet == false ) {
+        } else if (powerOutlet == false) {
             return true;
         } else {
             return false;
@@ -64,9 +64,9 @@ const Booking2 = (props) => {
 
     const checkMonitor = (currentTable) => {
 
-        if(monitor && currentTable.hasComputer  && currentTable.hasComputer == true) {
+        if (monitor && currentTable.hasComputer && currentTable.hasComputer == true) {
             return true;
-        } else if ( monitor == false ) {
+        } else if (monitor == false) {
             return true
         } else {
             return false;
@@ -75,9 +75,9 @@ const Booking2 = (props) => {
 
     const checkProjector = (currentTable) => {
 
-        if(projector && currentTable.hasProjector  && currentTable.hasProjector == true) {
+        if (projector && currentTable.hasProjector && currentTable.hasProjector == true) {
             return true;
-        } else if ( projector == false ) {
+        } else if (projector == false) {
             return true
         } else {
             return false;
@@ -86,7 +86,7 @@ const Booking2 = (props) => {
 
     const checkCapacity = (currentTable) => {
         const capacity = props.userOptions.people;
-        if(currentTable.capacity && currentTable.capacity == capacity ) {
+        if (currentTable.capacity && currentTable.capacity == capacity) {
             return true;
         } else {
             return false;
@@ -106,27 +106,27 @@ const Booking2 = (props) => {
 
         const currentBooking = await fireStore.getByQuery("bookings", [query]);
         const newArray = [];
-        if(currentBooking) {
+        if (currentBooking) {
             currentBooking.forEach((booking) => {
                 console.log(booking.data())
-                newArray.push({id: booking.id, ...booking.data()})
+                newArray.push({ id: booking.id, ...booking.data() })
             })
             setPreviousBooking(newArray);
         } else {
             console.log("No booking found")
-        }   
+        }
         return newArray;
     }
 
     const checkPreviousBooking = (table, prevBooking) => {
         console.log("check previous booking")
         // debugger;
-        for(let i = 0; i < prevBooking.length; i++) {
+        for (let i = 0; i < prevBooking.length; i++) {
 
             const previousBook = prevBooking[i];
-            if(previousBook.tableId == table.tableId) {
+            if (previousBook.tableId == table.tableId) {
 
-                if(previousBook.hours && previousBook.hours.length > 0 && bookingTime && bookingTime.length > 0) {
+                if (previousBook.hours && previousBook.hours.length > 0 && bookingTime && bookingTime.length > 0) {
 
                     const currentLow = bookingTime[0];
                     const currentHigh = bookingTime[bookingTime.length - 1];
@@ -135,24 +135,24 @@ const Booking2 = (props) => {
                     const previousLow = preHours[0];
                     const previousHigh = preHours[preHours.length - 1];
 
-                    if( (currentLow < previousLow && currentHigh < previousLow) || ( currentLow > previousHigh && currentHigh > previousHigh )) {
+                    if ((currentLow < previousLow && currentHigh < previousLow) || (currentLow > previousHigh && currentHigh > previousHigh)) {
                         return true
                     } else {
                         return false;
                     }
-                }  else {
+                } else {
                     return true
                 }
-                
+
             }
         }
         return true;
     }
 
     const applyFilter = (tables, previousBookingDirect = []) => {
-        const prevBooking = previousBooking.length > 0 ?  previousBooking : previousBookingDirect;
+        const prevBooking = previousBooking.length > 0 ? previousBooking : previousBookingDirect;
         let newTables = tables ? [...tables] : [...currentTables];
-        for(let i = 0; i < newTables.length; i++) { 
+        for (let i = 0; i < newTables.length; i++) {
 
             // Check previous booking
             const isAvailable = checkPreviousBooking(newTables[i], prevBooking);
@@ -163,7 +163,7 @@ const Booking2 = (props) => {
 
             // check Power Outlet
             const isPowerOutlet = checkPowerOutlet(newTables[i]);
-            
+
             // Check monitor
             const isMonitor = checkMonitor(newTables[i]);
 
@@ -172,29 +172,29 @@ const Booking2 = (props) => {
 
             // Check computers
 
-            
 
 
-            if(isPowerOutlet && isMonitor && projector && isCapacityMatch && isAvailable) {
+
+            if (isPowerOutlet && isMonitor && projector && isCapacityMatch && isAvailable) {
                 newTables[i].disabled = false
             } else {
-                newTables[i].disabled = true 
+                newTables[i].disabled = true
             }
         }
         setCurrentTables(newTables);
-        
+
     }
 
-    useEffect (() => {
-       applyFilter()
+    useEffect(() => {
+        applyFilter()
 
-    }, [powerOutlet, monitor, projector, bookingTime, computers ])
+    }, [powerOutlet, monitor, projector, bookingTime, computers])
 
 
 
     useEffect(() => {
         fetchTables();
-        
+
     }, [])
 
     useEffect(() => {
@@ -212,7 +212,7 @@ const Booking2 = (props) => {
                     if (!elm.classList.contains('selectedTime')) {
                         elm.classList.add('selectedTime');
                     }
-                    
+
                 })
             }
         }
@@ -221,7 +221,7 @@ const Booking2 = (props) => {
     window.dispatchEvent(new Event('resize'));
 
     const changeNameToInt = (tables, previousBooking = []) => {
-        for(let i = 0; i < tables.length; i++) {
+        for (let i = 0; i < tables.length; i++) {
             tables[i].name = parseInt(tables[i].name)
         }
         tables.sort((a, b) => {
@@ -234,24 +234,24 @@ const Booking2 = (props) => {
 
 
     const fetchTables = async () => {
-    
+
         let query = [
             {
-                "propertyName" : "locationId",
-                "operation" : "==",
-                "value" : props.userOptions.building
+                "propertyName": "locationId",
+                "operation": "==",
+                "value": props.userOptions.building
             }
         ];
 
-        if(props.userOptions.level != null && props.userOptions.level != undefined) {
+        if (props.userOptions.level != null && props.userOptions.level != undefined) {
             query.push({
-                "propertyName" : "floor",
-                "operation" : "==",
-                "value" : parseInt(props.userOptions.level)
+                "propertyName": "floor",
+                "operation": "==",
+                "value": parseInt(props.userOptions.level)
             })
         }
 
-        const tables = await fireStore.getByQuery('tables',  query);
+        const tables = await fireStore.getByQuery('tables', query);
         const newArray = []
         if (tables) {
             tables.forEach((table) => {
@@ -259,14 +259,14 @@ const Booking2 = (props) => {
             })
             const previousBooking = await getPreviousBooking();
             changeNameToInt(newArray, previousBooking)
-        }   
+        }
     }
 
     const spaceSelected = (e, id, index) => {
-        if(bookingTime.length == 0) {
+        if (bookingTime.length == 0) {
             return alert("Please choose the time slot")
         }
-        if(currentTables[index] && currentTables[index].disabled && currentTables[index].disabled == true) {
+        if (currentTables[index] && currentTables[index].disabled && currentTables[index].disabled == true) {
             return;
         }
         setSelectedTable(id)
@@ -281,20 +281,20 @@ const Booking2 = (props) => {
     }
 
     const changeBookingTime = (e, startTIme) => {
-        
+
         const previousIndex = bookingTime.indexOf(startTIme);
         const newArray = [...bookingTime];
-        if(previousIndex != -1) {
+        if (previousIndex != -1) {
             newArray.splice(previousIndex);
             e.currentTarget.classList.toggle('selectedTime');
             setBookingTime(newArray);
         } else {
-            if(bookingTime.length < 3) {
-                if(newArray.length == 0 || (startTIme == newArray[0] - 1 || startTIme == newArray[newArray.length - 1] + 1)) {
+            if (bookingTime.length < 3) {
+                if (newArray.length == 0 || (startTIme == newArray[0] - 1 || startTIme == newArray[newArray.length - 1] + 1)) {
                     e.currentTarget.classList.toggle('selectedTime')
                     newArray.push(startTIme);
-                    newArray.sort((a, b) => a-b)
-                    setBookingTime(newArray); 
+                    newArray.sort((a, b) => a - b)
+                    setBookingTime(newArray);
                 }
             }
         }
@@ -308,7 +308,7 @@ const Booking2 = (props) => {
                 userId: userId ? userId : 'abcd',
                 tableId: selectedTable,
                 date: props.userOptions.bookingDate,
-                locationId:  props.userOptions.building,
+                locationId: props.userOptions.building,
                 level: props.userOptions.level,
                 people: props.userOptions.people,
                 spaceType: props.userOptions.spaceType,
@@ -327,7 +327,7 @@ const Booking2 = (props) => {
             } else {
                 const saveData = await fireStore.addDataToCollection('bookings', obj);
             }
-            
+
             // navigator("/home");
             navigator("/booking-list-demo");
         }
@@ -337,7 +337,7 @@ const Booking2 = (props) => {
         if (!bookingTime) {
             alert('Please select Booking Time');
             return false;
-        } 
+        }
         if (!selectedTable) {
             alert('Please select the Space');
             return false;
@@ -358,22 +358,23 @@ const Booking2 = (props) => {
 
                         <form>
                             <label htmlFor="filter1">Computers</label>
-                            <div className="bookingFilter" id="filter1">
-                                <input type="radio" id="c1" name="radioComputer" value={1} checked={computers == 1} onChange={changeComputers}  />
-                                <label htmlFor="c1">1</label>
-                                <input type="radio" id="c2" name="radioComputer" value={2} checked={computers == 2} onChange={changeComputers} />
-                                <label htmlFor="c2">2</label>
-                                <input type="radio" id="c4" name="radioComputer" value={4} checked={computers == 4} onChange={changeComputers} />
-                                <label htmlFor="c4">4</label>
-                                <input type="radio" id="c8" name="radioComputer" value={8} checked={computers == 8} onChange={changeComputers} />
-                                <label htmlFor="c8">8</label>
+                            <div>
+
+
+                                <select className="bookingFilter" id="filter1">
+                                    <option value="" selected disabled hidden>Select Computers</option>
+                                    <option id="c1" name="radioComputer" value={1} checked={computers == 1} onChange={changeComputers}>One Computer</option>
+                                    <option id="c2" name="radioComputer" value={2} checked={computers == 2} onChange={changeComputers}>Two Computers</option>
+                                    <option id="c4" name="radioComputer" value={4} checked={computers == 4} onChange={changeComputers}>Four Computers</option>
+                                    <option id="c8" name="radioComputer" value={8} checked={computers == 8} onChange={changeComputers}>Eight Computers</option>
+                                </select>
                             </div>
                         </form>
                         <form>
                             <label htmlFor="filter2">Power Outlet</label>
                             <section className="bookingFilter" id="filter2">
                                 <label className="switch" htmlFor='powerOutlet'>
-                                    <input type="checkbox" id='powerOutlet' checked={powerOutlet} value={powerOutlet} onChange={(e) => { setPowerOutlets(e.target.checked)}} />
+                                    <input type="checkbox" id='powerOutlet' checked={powerOutlet} value={powerOutlet} onChange={(e) => { setPowerOutlets(e.target.checked) }} />
                                     <span className="slider round"></span>
                                 </label>
                             </section>
@@ -382,7 +383,7 @@ const Booking2 = (props) => {
                             <label htmlFor="filter3">Monitor</label>
                             <section className="bookingFilter" id="filter3">
                                 <label className="switch">
-                                    <input type="checkbox" checked={monitor} value={monitor} onChange={(e) => {setMonitor(e.target.checked)}} />
+                                    <input type="checkbox" checked={monitor} value={monitor} onChange={(e) => { setMonitor(e.target.checked) }} />
                                     <span className="slider round"></span>
                                 </label>
                             </section>
@@ -391,9 +392,10 @@ const Booking2 = (props) => {
                             <label htmlFor="filter4">Projector</label>
                             <section className="bookingFilter" id="filter4">
                                 <label className="switch">
-                                    <input type="checkbox" checked={projector} value={projector} onChange={(e) => { 
+                                    <input type="checkbox" checked={projector} value={projector} onChange={(e) => {
                                         console.log(e.target.checked);
-                                        setProjector(e.target.checked)}} />
+                                        setProjector(e.target.checked)
+                                    }} />
                                     <span className="slider round"></span>
                                 </label>
                             </section>
@@ -408,9 +410,9 @@ const Booking2 = (props) => {
                     </article>
                     <div>
                         {
-                            timeSlotes && timeSlotes.map((time,index) => {
+                            timeSlotes && timeSlotes.map((time, index) => {
                                 return (
-                                    <p className='timeSelected' id={`time-${time.startTime}`} key={`time${index}`} onClick={ (e) => {changeBookingTime(e, time.startTime)}}>
+                                    <p className='timeSelected' id={`time-${time.startTime}`} key={`time${index}`} onClick={(e) => { changeBookingTime(e, time.startTime) }}>
                                         {time.display}
                                     </p>
                                 )
@@ -427,21 +429,21 @@ const Booking2 = (props) => {
                     {
                         currentTables && currentTables.map((table, index) => {
                             return (
-                                <p 
-                                    className={"n"+table.capacity + " " + `${selectedTable === table.tableId ? "selected" : ""}` + " " + `${table.disabled == true ? "disabled" : "notDisabled"}`}
-                                    id={'s'+ table.name} 
+                                <p
+                                    className={"n" + table.capacity + " " + `${selectedTable === table.tableId ? "selected" : ""}` + " " + `${table.disabled == true ? "disabled" : "notDisabled"}`}
+                                    id={'s' + table.name}
                                     key={table.id}
-                                    onClick={(e) => {  spaceSelected(e, table.tableId, index) }}
+                                    onClick={(e) => { spaceSelected(e, table.tableId, index) }}
                                 >
                                     {
-                                        table.capacity == 1 ? <img src={singleLogo}/> : (
-                                            table.capacity == 2 ? <img src={TwoLogo}/>: (
-                                                table.capacity == 4 ? <img src={FourLogo}/> : <img src={MultipleLogo}/>
+                                        table.capacity == 1 ? <img src={singleLogo} /> : (
+                                            table.capacity == 2 ? <img src={TwoLogo} /> : (
+                                                table.capacity == 4 ? <img src={FourLogo} /> : <img src={MultipleLogo} />
                                             )
-                                            
-                                            )
+
+                                        )
                                     }
-                                    
+
                                 </p>
                             )
                         })
