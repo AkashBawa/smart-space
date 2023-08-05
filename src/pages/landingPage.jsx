21e6669d01ed36d1007c78b66b34db234efed2a6
Ahmed Abdou <hakam4all@yahoo.com>
2023-08-05 10:44:25 -0700

    font update

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -2,20 +2,7 @@ const LandingPage = () => {
     return (
         <div className="landingWrapper">
             <h1>Landing page</h1>
-
-
-            {/* Need to add this Dans google fonts to the HTML:
-            
-            
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==' crossorigin='anonymous' referrerpolicy='no-referrer' />
-
-
-            <style>
-        @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap');
-             </style>
-
-             
-            */}
+           
         </div>
     )
 }
